20c5677086c1ffdd3b0d576ab600421aa20044cd
GABRIELDELASSIM <143609610+gabrieldelassim@users.noreply.github.com>
2025-04-08 21:03:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+const alternaContraste = document.getElementById('alterna-contraste');
+const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+// Alternar alto contraste
+alternaContraste.addEventListener('click', function () {
+  document.body.classList.toggle('alto-contraste');
+});
+
+// Alternar exibição do menu e aria-expanded
+botaoDeAcessibilidade.addEventListener('click', function () {
+  botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+  opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+
+  const aberto = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+  botaoDeAcessibilidade.setAttribute('aria-expanded', !aberto);
+});
